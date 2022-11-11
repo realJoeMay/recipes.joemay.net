9ef81f56e147069c19ca0b5defc8eb7bd3ae07ff
realJoeMay <80790105+realJoeMay@users.noreply.github.com>
2022-11-10 19:08:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,46 @@ function showIngredientCost() {
 function showIngredientNone() {
     document.querySelector("body").classList.remove("show-ingredient-cost");
 }
+
+
+
+// Theme
+
+function themeDark() {
+    clearTheme()
+    document.querySelector("body").classList.add("theme-dark");
+    setCookie("theme", "dark", 365)
+}
+
+function themeLight() {
+    clearTheme()
+    document.querySelector("body").classList.add("theme-light");
+    setCookie("theme", "light", 365)
+}
+
+function themeSystem() {
+    clearTheme()
+    document.querySelector("body").classList.add("theme-system");
+    setCookie("theme", "system", 365)
+}
+
+function clearTheme() {
+    document.querySelector("body").classList.remove("theme-dark");
+    document.querySelector("body").classList.remove("theme-light");
+    document.querySelector("body").classList.remove("theme-system");
+}
+
+function checkTheme() {
+    console.log("Checking theme now");
+    if (getCookie("theme") == "dark") {
+        document.querySelector("#theme-dark").checked = true;
+        themeDark();
+    } else if (getCookie("theme") == "light") {
+        document.querySelector("#theme-light").checked = true;
+        themeLight(); 
+    } else {
+        document.querySelector("#theme-system").checked = true;
+        themeSystem();
+    }
+}
+checkTheme()
