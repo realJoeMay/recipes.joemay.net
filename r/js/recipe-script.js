3559930b3cc303f70b960223ca7fe9a76e99b5ed
realJoeMay <80790105+realJoeMay@users.noreply.github.com>
2023-08-19 04:41:48 -0400

    Add files via upload

--- r/js/recipe-script.js
+++ r/js/recipe-script.js
@@ -42,16 +42,47 @@ function closeMenu() {
     document.querySelector("body").classList.remove("menu-open");
 }
 
+// fix menu slides out on initial load
+setTimeout(function() {
+    document.querySelector("body").classList.remove("loading");
+}, 1000)
+
+
+
+
+// Togggle Menu Items
+
+function toggleAppearance() {
+    if (document.querySelector("body").classList.contains("menu-appearance-open")) {
+        hideMenuAppearance();
+    } else {
+        showMenuAppearance();
+    }
+}
+
+function showMenuAppearance() {
+    document.querySelector("body").classList.add("menu-appearance-open");
+}
+
+function hideMenuAppearance() {
+    document.querySelector("body").classList.remove("menu-appearance-open");
+}
+
+
+
+
+
+
 
 // ingredient details
 
-function showIngredientCost() {
-    document.querySelector("body").classList.add("show-ingredient-cost");
-}
+// function showIngredientCost() {
+//     document.querySelector("body").classList.add("show-ingredient-cost");
+// }
 
-function showIngredientNone() {
-    document.querySelector("body").classList.remove("show-ingredient-cost");
-}
+// function showIngredientNone() {
+//     document.querySelector("body").classList.remove("show-ingredient-cost");
+// }
 
 
 // Theme
@@ -97,9 +128,30 @@ checkTheme()
 
 
 
-// fix menu slides out on initial load
-setTimeout(function() {
-    document.querySelector("body").classList.remove("loading");
-}, 1000)
 
+
+// Copy Ingredients
+var copyButton = document.getElementById("copy-ingredients-btn");
+var copyButtonText = document.getElementById("copy-ingredients-btn-text");
+var textToCopy = document.getElementById("ingredients-text");
+
+copyButton.addEventListener("click", function() {
+    var copiedText = "";
+    textToCopy.querySelectorAll("li").forEach(function(line) {
+        copiedText += line.textContent.trim() + "\n";
+    });
+
+    // Copy the text to the clipboard
+    navigator.clipboard.writeText(copiedText)
+        .then(function() {
+            // Change the button text briefly to indicate copy
+            copyButtonText.textContent = "Copied!";
+            setTimeout(function() {
+                copyButtonText.textContent = "Copy Ingredients";
+            }, 3000);
+        })
+        .catch(function(err) {
+            console.error('Unable to copy text: ', err);
+        });
+});
 
